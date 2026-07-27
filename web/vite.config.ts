7c5ee4f4889f29c62def2fcc4773a12ba092cd96
Chris <cdameron101@yahoo.com>
2026-07-27 18:08:36 -0500

    Fix GitHub Pages deployment

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5176,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5177',
-        changeOrigin: true
-      }
-    }
-  }
+  base: "/news-reader/",
 });
